e74fce0702a59454af61e8d065b62149c691a8e5
hanzalah5 <hanzalah435@gmail.com>
2026-05-04 10:22:13 +0500

    initial commit

---
+++ agent/chatapp/memory_callbacks.py
@@ -0,0 +1,5 @@
+from google import adk
+
+async def auto_save_session_to_memory_callback(callback_context):
+    await callback_context._invocation_context.memory_service.add_session_to_memory(
+        callback_context._invocation_context.session)
